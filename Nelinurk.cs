590a6958c0df8bafe4c5b8a7e71df98f1f9dd0ea
opilane <opilane@HP-CZC6297B8P>
2022-03-02 10:55:11 +0200

    Add project files.

---
+++ Nelinurk.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Kollitoo
+{
+    class Nelinurk
+    {
+        int korgus;
+        int laius;
+
+     
+
+        public int Korgus
+        {
+            set
+            {
+                 korgus = value;
+            }
+            get { return korgus; }
+        }
+        public int Laius
+        {
+            set
+            {
+                laius = value;
+            }
+            get { return laius; }
+        }
+
+        
+
+        public void Umbermoot()
+        {
+            int a = korgus;
+            int b = laius;
+            int c = (a + b) * 2;
+            Console.WriteLine($"Minu ümbermõõt on {c} ");
+        }
+
+        public void Pindala()
+        {
+            int a = korgus;
+            int b = laius;
+            int c = (a * b) ;
+            Console.WriteLine($"Minu pindala on {c} ");
+        }
+    }
+}
+
+    
+
---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Kollitoo
+{
+    class Program
+    {
+
+
+
+        static void Main(string[] args)
+        {
+            Nelinurk a = new Nelinurk();
+            a.Korgus = int.Parse(Console.ReadLine());
+            a.Laius = int.Parse(Console.ReadLine());
+            
+            a.Umbermoot();
+            a.Pindala();
+        }
+        
+    }
+}
